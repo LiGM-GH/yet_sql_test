86fe2572235232c5eb32182cfb574fcb38d62d00
LiGM-GH <lipkin.grisha@mail.ru>
2026-05-25 03:35:38 +0300

    Implemented clients-with-overdue-payments and TOP-15

--- queries.sql
+++ queries.sql
@@ -51,3 +51,52 @@ FROM
   cards_above_x,
   given_credits,
   given_credits_above_x;
+
+SELECT
+  name,
+  surname,
+  patronymic,
+  credit_type,
+  loan_amount
+FROM
+  Credit_Info
+  JOIN Customer_Info cust USING (customer_id)
+  JOIN Payments_Info USING (credit_id)
+WHERE
+  payment_date >= '2022-10-01'
+  AND delay_days > 0
+  AND loan_amount > 100000
+  AND (
+    SELECT
+      COUNT(application_status)
+    FROM
+      cards_info
+      JOIN Customer_Info USING (customer_id)
+    WHERE
+      application_status = 'A'
+      AND customer_id = cust.customer_id
+  ) = 0;
+
+SELECT
+  shop_name,
+  credits_num,
+  summary_loan,
+  average_loan
+FROM
+  (
+    SELECT
+      shop_name,
+      COUNT(credit_type) AS credits_num,
+      SUM(loan_amount) AS summary_loan,
+      AVG(loan_amount) AS average_loan
+    FROM
+      Credit_Info
+    WHERE
+      credit_status = '1'
+    GROUP BY
+      shop_name
+  )
+ORDER BY
+  credits_num
+LIMIT
+  15;
